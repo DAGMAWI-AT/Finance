70c38695ccac2aa4d5aaeb4c29400001e383d7bd
DAGMAWI-AT <vvman1717@gmail.com>
2025-03-28 00:04:22 -0700

    for server

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -81,13 +81,13 @@ async function login(req, res) {
 
     // Cookie options
     const cookieOptions = {
-      httpOnly: true, // Prevent access by JavaScript
-      secure: process.env.NODE_ENV === "production", // Send only over HTTPS in production
-      sameSite: "None", // For cross-site requests, "None" requires secure:true
+      httpOnly: true, 
+      secure: process.env.NODE_ENV === "production", 
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // For cross-site requests, "None" requires secure:true
       // sameSite: "Strict", // Prevent CSRF
       maxAge: 60 * 60 * 1000, // 1 hour
       // domain: "https://finance-production-d89b.up.railway.app",
-      path: "/", // Accessible across the entire site
+      path: "/", 
     };
 
     // Set cookie
