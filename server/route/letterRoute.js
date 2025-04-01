1002df3cae11d526036283b28182faccdac538bb
DAGMAWI-AT <vvman1717@gmail.com>
2025-04-01 03:52:47 -0700

    commit

--- server/route/letterRoute.js
+++ server/route/letterRoute.js
@@ -12,7 +12,7 @@ const {
 const verifyToken = require("../middleware/authMiddleware");
 
 // POST: Create a letter
-router.post("/submit", verifyToken, uploadMiddleware.single('attachment'), createLetter);
+router.post("/submit", verifyToken, uploadMiddleware, createLetter);
 
 // GET: Retrieve all letters
 router.get('/', getAllLetters);
@@ -22,7 +22,7 @@ router.get('/get/:id', getLetterById);
 // router.get('/:id', getLetterById);
 
 // PUT: Update a letter by ID
-router.put('/:id', verifyToken, uploadMiddleware.single('attachment'), updateLetter);
+router.put('/:id', verifyToken, uploadMiddleware, updateLetter);
 
 // DELETE: Delete a letter by ID
 router.delete('/:id', verifyToken, deleteLetter);
@@ -31,32 +31,3 @@ router.get('/cso/:csoId', verifyToken, getLettersByCSO);
 module.exports = router;
 
 
-// const express = require('express');
-// const router = express.Router();
-// const {
-//     createLetter,
-//     getAllLetters,
-//     getLetterById,
-//     updateLetter,
-//     deleteLetter,
-//     uploadMiddleware
-// } = require('../controller/letterController');  // Assuming your controller file is named letterController.js
-// const verifyToken = require("../middleware/authMiddleware");
-// // const letterController = require('../controller/letterController');
-
-// // POST: Create a letter
-// router.post("/letters", verifyToken, uploadMiddleware, createLetter);
-
-// // GET: Retrieve all letters
-// router.get('/letters', getAllLetters);
-
-// // GET: Retrieve a single letter by ID
-// router.get('/letters/:id', getLetterById);
-
-// // PUT: Update a letter by ID
-// router.put('/letters/:id', uploadMiddleware, updateLetter);
-
-// // DELETE: Delete a letter by ID
-// router.delete('/letters/:id', deleteLetter);
-
-// module.exports = router;
